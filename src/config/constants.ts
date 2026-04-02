eb49426f627ec5a3f11dbb5345467675d7981e31
git-akihakune <akihakune@protonmail.ch>
2026-04-02 20:57:28 +0000

    feat: add game constants and score thresholds

---
+++ src/config/constants.ts
@@ -0,0 +1,64 @@
+// Canvas
+export const CANVAS_WIDTH = 800;
+export const CANVAS_HEIGHT = 300;
+export const GROUND_Y = 240;
+
+// Dino
+export const DINO_X = 80;
+export const DINO_WIDTH = 44;
+export const DINO_HEIGHT = 48;
+export const DINO_DUCK_HEIGHT = 30;
+
+// Physics
+export const GRAVITY = 2200;
+export const JUMP_VELOCITY = -680;
+export const INITIAL_SPEED = 350;
+export const MAX_SPEED = 1200;
+export const SPEED_ACCEL = 8;
+
+// Obstacles
+export const MIN_OBSTACLE_GAP = 350;
+export const MAX_OBSTACLE_GAP = 700;
+export const CACTUS_SM = { width: 24, height: 50 };
+export const CACTUS_MD = { width: 40, height: 60 };
+export const CACTUS_LG = { width: 52, height: 70 };
+export const BIRD_SIZE = { width: 50, height: 30 };
+export const MAX_BIRD_PROBABILITY = 0.35;
+
+// Score thresholds for evolution
+export const THRESHOLD_COLOR_TINT = 200;
+export const THRESHOLD_SKY_COLOR = 400;
+export const THRESHOLD_PLATFORMS = 600;
+export const THRESHOLD_FLYING_ENEMIES = 800;
+export const THRESHOLD_FULL_COLOR = 1000;
+export const THRESHOLD_COLLECTIBLES = 1200;
+export const THRESHOLD_POWERUPS = 1500;
+export const THRESHOLD_LANES = 2000;
+export const THRESHOLD_BOSS_PATTERNS = 2500;
+
+// Auto-play
+export const AUTOPLAY_REACT_MS = 310;
+export const AUTOPLAY_POWERUP_DURATION = 10000;
+export const AUTOPLAY_UNLOCK_SCORE = 2500;
+
+// Power-ups
+export const SHIELD_DURATION = 0;
+export const MAGNET_DURATION = 8000;
+export const MAGNET_RADIUS = 120;
+
+// Lanes
+export const LANE_COUNT = 3;
+export const LANE_POSITIONS = [160, 200, 240];
+
+// Clouds
+export const CLOUD_MIN_Y = 40;
+export const CLOUD_MAX_Y = 120;
+export const CLOUD_SPEED_FACTOR = 0.3;
+
+// Colors
+export const MONO_COLOR = 0x535353;
+export const MONO_BG = 0xf7f7f7;
+
+// Platform
+export const PLATFORM_WIDTH = 100;
+export const PLATFORM_HEIGHT = 16;
